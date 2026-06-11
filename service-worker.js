6e55b963c8936269201146d280fc06df4abb55d7
messolarllc <122553179+messolarllc@users.noreply.github.com>
2026-06-10 19:28:04 -0500

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // Service Worker — cachea los archivos para que la app funcione sin internet
-const CACHE_NAME = 'mudanzas-v3';
+const CACHE_NAME = 'mudanzas-v4';
 const ASSETS = [
   './',
   './index.html',
